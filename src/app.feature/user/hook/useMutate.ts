6ace1a8fe9441771103b73421c7e955d76c6255e
hazzzi <hanee_l@naver.com>
2024-10-15 22:50:14 +0900

    refactor: 데이터 fetching 로직을 추상화

---
+++ src/app.feature/user/hook/useMutate.ts
@@ -0,0 +1,29 @@
+import { BASE_URL } from '@/app.module/api/environment';
+import axios from 'axios';
+import { useState } from 'react';
+
+function useMutate<T>(url: string, method: 'post' | 'put' | 'delete') {
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const mutate = async (newData: T, onSuccess?: () => void) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      if (method === 'put') await axios.put(`${BASE_URL}${url}`, newData);
+      if (method === 'delete') await axios.delete(`${BASE_URL}${url}`);
+      if (method === 'post') await axios.post(`${BASE_URL}${url}`, newData);
+
+      onSuccess?.();
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { error, loading, mutate };
+}
+
+export default useMutate;
